cb24fce3eb4379b5722a4031f8a1d20330428907
Shah Abdul Ghani <abdul.ghani@hashedin.com>
2019-12-05 16:25:18 +0530

    Changed the icons for PWA and the footer text.

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const breakpoints = {
   md: 768
 };
 
-export const FOOTER_TEXT = `Sha's Pet Boarding Services ©2019`;
+export const FOOTER_TEXT = `Claws and Paws Pet Boarding Services ©2019`;
 
 export const HEADER_TEXT = `Sha's Boarding Services`;
 
